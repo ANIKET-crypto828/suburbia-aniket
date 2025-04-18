947c0afda93e5360a0497769eb0ce332b67c9e4f
ANIKET-crypto828 <jhumasantra86@gmail.com>
2025-04-18 23:13:55 +0530

    fix

--- src/components/Bounded.tsx
+++ src/components/Bounded.tsx
@@ -2,7 +2,7 @@ import { CSSProperties, ElementType, ReactNode } from "react";
 import clsx from "clsx";
 
 type BoundedProps = {
-  as?: ElementType;
+  as?: ElementType<{ children?: ReactNode }>;
   className?: string;
   style?: CSSProperties;
   children: ReactNode;
@@ -18,7 +18,7 @@ export function Bounded({
     <Comp
       className={clsx(
         "px-6 ~py-10/16 [.header+&]:pt-44 [.header+&]:md:pt-32",
-        className,
+        className
       )}
       {...restProps}
     >
